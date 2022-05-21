7a26ac14ec08973e5a96cae07c8a44576462f0f8
Whal3 <35258105+leehan416@users.noreply.github.com>
2022-05-21 04:14:34 +0000

    100문제

---
+++ 백준/2747.py
@@ -0,0 +1,13 @@
+l= [0]*91
+def fibo(n): 
+  if l[n]!=0:
+    return l[n]
+  if n == 0:
+    return 0
+  elif n <= 2:
+    return 1
+  else:
+    l[n]= fibo(n-1)+fibo(n-2)
+    return l[n]
+print(fibo(int(input())))
+# 2748과 동일한 코드 
