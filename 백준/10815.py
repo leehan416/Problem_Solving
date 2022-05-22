0076274828a6aef136c87dfe51d886bd2e9c0a74
Whal3 <35258105+leehan416@users.noreply.github.com>
2022-05-22 05:26:51 +0000

    일요일 오후 문제풀이

---
+++ 백준/10815.py
@@ -0,0 +1,15 @@
+n = input()
+a = list(map(int, input().split()))
+m = input()
+l = list(map(int, input().split()))
+a.sort()
+
+def search(f, b, n):
+  if f > b: return 0
+  m = (f + b) // 2 
+  if a[m] == n: return 1
+  elif n > a[m]: return search(m + 1, b, n)
+  else: return search(f, m - 1, n)
+
+for i in l:
+  print(search(0, len(a)-1, i), end = ' ') 
