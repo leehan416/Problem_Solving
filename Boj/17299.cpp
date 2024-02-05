c10f1f8d80fa5e63bbc2b7f4358178dc42fee2ad
Whal3 <leehan416@gmail.com>
2024-02-05 23:23:45 +0900

    [Boj]17299_테스트중

---
+++ Boj/17299.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    int ngf[1000001] = {0};
+    int F[1000001] = {0};
+    int arr[1000001] = {0};
+    
+    stack<int> stk;
+    int n;
+
+    cin >> n;
+
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        F[arr[i]]++;
+    }
+
+    for (int i = n - 1; i >= 0; i--){
+        while(1) {
+            if (stk.empty()) {
+                ngf[i] = -1;
+                break;
+            } else {
+                if (F[stk.top()] > F[arr[i]]){
+                    ngf[i] = stk.top();
+                    stk.push(arr[i]);
+                    break;
+                } else {
+                    stk.pop(); 
+                }
+            }
+        }
+
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << ngf[i] << ' ';
+    }
+
+
+
+
+
+    
+    return 0;
+}
