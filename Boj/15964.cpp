07981b5644282a67359cf7f0e39ef0919eba9892
Whal3 <leehan416@gmail.com>
2024-11-16 16:42:02 +0900

    [BOJ] 15964, 27866

---
+++ Boj/15964.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    long long a, b;
+    cin >> a >> b;
+
+
+    long long value = (a+b) * (a-b);
+
+
+    cout << value << endl;
+
+    return 0;
+}
